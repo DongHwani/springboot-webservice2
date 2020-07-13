98de8b26a8a3e121d03dff36a9a31a0f6ce73b5a
DongHwan Kim <bkuekany@gmail.com>
2020-07-13 23:50:42 +0900

    제 3장 완료

---
+++ src/main/java/com/dhk/tutorial/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.dhk.tutorial.domain.posts;
+
+import com.dhk.tutorial.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
