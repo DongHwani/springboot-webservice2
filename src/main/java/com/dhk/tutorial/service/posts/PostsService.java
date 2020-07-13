ddc5e5ec87b81824db78d7ff1c4402326b887b3b
DongHwan Kim <bkuekany@gmail.com>
2020-07-14 00:16:27 +0900

    4장 완료

--- src/main/java/com/dhk/tutorial/service/posts/PostsService.java
+++ src/main/java/com/dhk/tutorial/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.dhk.tutorial.service.posts;
 
 import com.dhk.tutorial.domain.posts.Posts;
 import com.dhk.tutorial.domain.posts.PostsRepository;
+import com.dhk.tutorial.web.dto.PostsListResponseDto;
 import com.dhk.tutorial.web.dto.PostsResponseDto;
 import com.dhk.tutorial.web.dto.PostsSaveRequestDto;
 import com.dhk.tutorial.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,21 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("해당게시글이 없습니다. id = "+id));
+
+        postsRepository.delete(posts);
     }
 }
